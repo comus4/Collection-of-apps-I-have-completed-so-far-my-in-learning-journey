1570eae16ecf89581c1031ec74524e6aed5d2f0a
Comus Hardman <comuseh@gmail.com>
2022-07-03 11:52:49 -0400

    Initial Commit

---
+++ Project13/Project13App.swift
@@ -0,0 +1,17 @@
+//
+//  Project13App.swift
+//  Project13
+//
+//  Created by Comus Hardman IV on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Project13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
