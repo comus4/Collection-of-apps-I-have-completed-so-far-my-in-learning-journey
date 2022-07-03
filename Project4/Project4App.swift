19aaef68dd77ffc68d65117487be3dedca96dc10
Comus Hardman <comuseh@gmail.com>
2022-07-03 09:33:26 -0400

    Initial Commit

---
+++ Project4/Project4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4App.swift
+//  Project4
+//
+//  Created by Comus Hardman IV on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Project4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
