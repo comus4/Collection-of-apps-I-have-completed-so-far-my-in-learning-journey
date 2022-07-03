17af6cf8a51e59da00443e2ec584eafdb217eff2
Comus Hardman <comuseh@gmail.com>
2022-07-03 11:03:44 -0400

    Initial Commit

---
+++ Project10/Project10App.swift
@@ -0,0 +1,17 @@
+//
+//  Project10App.swift
+//  Project10
+//
+//  Created by Comus Hardman IV on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Project10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
