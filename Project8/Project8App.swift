392636cf3034746cf50c09fc4049cba438bf0218
Comus Hardman <comuseh@gmail.com>
2022-07-03 10:29:54 -0400

    Initial Commit

---
+++ Project8/Project8App.swift
@@ -0,0 +1,17 @@
+//
+//  Project8App.swift
+//  Project8
+//
+//  Created by Comus Hardman IV on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Project8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
