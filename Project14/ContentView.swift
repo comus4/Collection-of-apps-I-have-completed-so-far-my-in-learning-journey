af117359268e91ffe5c3a70b1dc5e4f6a3a25afb
Comus Hardman <comuseh@gmail.com>
2022-07-03 14:28:55 -0400

    Initial Commit

---
+++ Project14/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project14
+//
+//  Created by Comus Hardman IV on 7/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
