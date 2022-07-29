b56dd12172be1720c6664fb47432690a88109363
Comus Hardman <comuseh@gmail.com>
2022-07-29 17:11:14 -0400

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Comus Hardman IV on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
