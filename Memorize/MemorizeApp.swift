84ce19e88894886c11eaa5fd786844cbc3cca3c4
Comus Hardman <comuseh@gmail.com>
2022-11-22 11:05:52 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Comus Hardman IV on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
